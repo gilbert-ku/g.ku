b4ecdaa113e2fa6fc652404a24836d96be342359
gilbert-ku <gilbert45ku@gmail.com>
2024-07-31 00:32:36 +0300

    add blogs page

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import About from "./about/page";
+import Blogs from "./blogs/page";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,6 +16,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         {children}
         <About />
+        <Blogs />
       </body>
     </html>
   );
