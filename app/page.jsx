3fc2bc1b4cc49e62d86ba7104b4102b498923b23
gilbert-ku <gilbert45ku@gmail.com>
2024-08-02 00:33:59 +0300

    add home-bg, profile image and set background image

--- app/page.jsx
+++ app/page.jsx
@@ -1,15 +1,24 @@
+import Image from "next/image";
 
 export default function Home() {
   return (
-    // <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    // {/* // <main> */}
+    // <main className=" home-pg flex min-h-screen flex-col items-center justify-between" >
+    <main className=" home-pg  min-h-screen "> 
+      <div className="flex justify-around bg-transparent">
+        
 
-    //   <div>
-    //     <h1>Hello Gilbert. welcome to my new portfolio</h1>
-    //   </div>
-    // </main>
+        <Image 
+        src="/profile-image.png"
+        width={500}
+        height={500}
+        alt="profile image"
+        
+        />
 
-    <>
-    </>
+
+      </div>
+    </main>
+
+    
   );
 }
