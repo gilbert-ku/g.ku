1b4dabcc0e531d0dbbad58f8b8003f6fab22cbda
gilbert-ku <gilbert45ku@gmail.com>
2024-08-05 23:30:26 +0300

    update auto type and style the home page

--- app/page.jsx
+++ app/page.jsx
@@ -13,23 +13,33 @@ export default function Home() {
     <main className="home-pg  min-h-screen text-white"> 
       <div className="md:flex justify-evenly my-12">
         <div className=" w-1/2">
-          <h1>Hello, My Name is,</h1>
+          <h1 className="text-2xl">Hello, My Name is,</h1>
 
-          <h1 className="text-2xl md:text-6xl font-bold">Gilbert <span className="text-light-blue-400" >Kutoto</span></h1>
+          <h1 className="text-2xl md:text-6xl font-bold">Gilbert <span className="text-light-blue-400" >Kutoto,</span></h1>
 
           <TypeAnimation
-            sequence={['Software Engineer.', 500, 'UX/UI Designer.', 500, 'Web Developer.', 500, 'Android/IOS Developer.', 500]}
+            sequence={[
+              // Same substring at the start will only be typed once, initially
+              'I am a Software Engineer,',
+              1000,
+              'I am a UX/UI Designer,',
+              1000,
+              'I am a Web Developer,',
+              1000,
+              'I am an Android/iOS Developer.',
+              1000,
+            ]}
+            speed={40}
             // style={{ fontSize: '2em' }}
-            className="md:text-4xl"
+            className="md:text-4xl my-2"
             repeat={Infinity}
+          />
 
-          /> 
-
-          <p>From Nairobi Kenya,I have a rich experience in development cycle form dynamic Web Application, 
+          <p className="text-2xl my-5">From Nairobi Kenya,I have a rich experience in development cycle form dynamic Web Application, 
             Mobile App development and I am also proficient in UX/UI design.
           </p>
 
-          <button className="p-2 bg-light-blue-600 rounded-md font-semibold">Download Resume</button>
+          <button className="p-2 bg-light-blue-600 rounded-md font-semibold my-5 px-5">Download Resume</button>
 
           <div className="flex text-blue-500">
           <FaGithub className="me-3 text-2xl text-blue-500"/>
