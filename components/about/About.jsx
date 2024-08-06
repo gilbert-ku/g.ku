d9be4eeda634e293ef22534e0225139cb69c31fa
gilbert-ku <gilbert45ku@gmail.com>
2024-08-06 10:06:24 +0300

    add about iage

--- components/about/About.jsx
+++ components/about/About.jsx
@@ -1,10 +1,18 @@
+import Image from 'next/image'
 import React from 'react'
 
 function About() {
   return (
     <>
         <div>
-            <h1>hello Kenya</h1>
+            <Image 
+                src="/about-image.png"
+                width={450}
+                height={450}
+                alt="profile image"
+                className="px-5 md:pr-0 mt-5 lg:mt-0 "
+                loading="lazy"
+            />
         </div>
     </>
   )
