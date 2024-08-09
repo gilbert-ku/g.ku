19450d4322d55bfe2d1bed409fb7866c88d9ecfd
gilbert-ku <gilbert45ku@gmail.com>
2024-08-09 12:41:28 +0300

    add projectlist  page

--- components/projects/Projects.jsx
+++ components/projects/Projects.jsx
@@ -1,13 +1,14 @@
 
-
-
 import React from 'react'
+import { ProjectsLists } from './ProjectsLists'
 
 const Projects = () => {
   return (
     <>
       <div className='text-white container mx-auto'>
-        <h1 className='text-3xl font-semibold font-serif'>Projects</h1>
+        <h1 className='text-3xl font-semibold font-serif mb-8'>Projects</h1>
+
+        <ProjectsLists />
       </div>
     </>
   )
---
+++ components/projects/ProjectsLists.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const ProjectsLists = () => {
+  return (
+    <>
+        <section className='text-white'>
+            <h1>projects lists</h1>
+        </section>
+    </>
+  )
+}
