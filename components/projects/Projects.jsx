3d393c0ef89d318a702956ace95ad43efc7d9675
gilbert-ku <gilbert45ku@gmail.com>
2024-08-14 13:05:27 +0300

    add style to project headline

--- components/projects/Projects.jsx
+++ components/projects/Projects.jsx
@@ -6,8 +6,11 @@ const Projects = () => {
   return (
     <>
       <div className='text-white container mx-auto'>
-        <h1 className='text-3xl font-semibold font-serif mb-8'>Projects</h1>
-
+        <h1 className=' mx-5 md:text-center text-xl md:text-2xl lg:text-3xl my-5 font-semibold'>
+          Latest Works 
+          <br />
+          Explore My Popular <span className='text-light-blue-600 '>Projects</span>
+        </h1>
         <ProjectsLists />
       </div>
     </>
