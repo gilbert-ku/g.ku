b873850aa37537bb4e208e67d5a996836776fd0c
gilbert-ku <gilbert45ku@gmail.com>
2024-08-14 15:19:25 +0300

    add vector for bg form

--- components/contact/ContactMe.jsx
+++ components/contact/ContactMe.jsx
@@ -1,13 +1,14 @@
 "use client"
+import Image from "next/image";
 import { FaGithub, FaLinkedinIn, FaTwitter, FaWhatsapp } from "react-icons/fa6";
 import { MdEmail } from "react-icons/md";
 
 const ContactMe = () => {
   return (
     <>
-        <section className='mx-24 bg-gray-900 rounded-lg text-white mt-10'>
+        <section className='mx-5 md:mx-12 lg:mx-24 bg-gray-900 rounded-lg text-white mt-10 md:flex'>
 
-            <div className="mx-5 w-1/3 py-5"> 
+            <div className="mx-5 md:w-1/3 py-5"> 
                 <h1 className="font-serif text-2xl">Get in Touch.</h1>
                 <h1 className="font-serif text-3xl font-semibold">Let’s Talk For your <span className="text-light-blue-600">Next Projects</span></h1>
                 <p className="md:text-xl">To discuss a project or just to say hi, feel free to connect with me via email, phone call, or through my social media handles.</p>
@@ -16,14 +17,24 @@ const ContactMe = () => {
 
                 {/* icons */}
                 <div className="flex text-blue-500 mt-1 md:mt-3">
-                <FaGithub className="me-2 md:me-3 text-2xl text-blue-500"/>
-                <FaLinkedinIn className="mx-2 md:mx-3 text-2xl md:text-3xl"/>
-                <FaTwitter className="mx-2 md:mx-3 text-2xl md:text-3xl"/>
-                <FaWhatsapp className="mx-2 md:mx-3 text-2xl md:text-3xl"/>
-                <MdEmail className="ms-2 md:ms-3 text-2xl md:text-3xl "/>
-              </div>
+                    <FaGithub className="me-2 md:me-3 text-2xl text-blue-500"/>
+                    <FaLinkedinIn className="mx-2 md:mx-3 text-2xl md:text-3xl"/>
+                    <FaTwitter className="mx-2 md:mx-3 text-2xl md:text-3xl"/>
+                    <FaWhatsapp className="mx-2 md:mx-3 text-2xl md:text-3xl"/>
+                    <MdEmail className="ms-2 md:ms-3 text-2xl md:text-3xl "/>
+                </div>
             </div>
 
+            <div>
+                <Image 
+                    src="/Vector.png"
+                    width={450}
+                    height={450}
+                    alt="profile image"
+                    className="px-5 md:pr-0 mt-5 lg:mt-0 items-center"
+                    loading="lazy"
+                />
+            </div>
         </section>
     </>
   )
