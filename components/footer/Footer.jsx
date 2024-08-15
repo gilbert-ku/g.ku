9a6cfa109d88a55266a7dd4f3c8e99fac33f5c0c
gilbert-ku <gilbert45ku@gmail.com>
2024-08-15 22:51:29 +0300

    update footer

--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -2,11 +2,8 @@ import React from 'react'
  const Footer = () => {
   return (
     <>
-    <footer className='md:flex justify-between items-center text-white font-serif text-xl md:text-3xl  bg-gray-900 mt-12'>
+    <footer className='md:flex justify-between items-center text-white font-serif md:text-xl  bg-gray-900 mt-12'>
         <p className='font-semibold mx-5 md:ms-12 text-center'>copyright @2024, <span className='text-light-blue-600 '>Gilbert Kutoto.</span></p>
-
-
-
         <div>
             <ul className='flex justify-evenly mx-7'>
                 <li className='px-3'>About</li>
