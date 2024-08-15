a87ada8bd2cd38c3c1e7af86ccc0a15b0af550b2
gilbert-ku <gilbert45ku@gmail.com>
2024-08-15 13:11:35 +0300

    add responsiveness to contact

--- components/contact/ContactMe.jsx
+++ components/contact/ContactMe.jsx
@@ -1,37 +1,36 @@
 
 import { FaGithub, FaLinkedinIn, FaTwitter, FaWhatsapp } from "react-icons/fa6";
-import { MdEmail, MdCall, MdOutlineMarkAsUnread } from "react-icons/md";
+import { MdCall, MdOutlineMarkAsUnread } from "react-icons/md";
 import Form from "./Form";
 
 const ContactMe = () => {
   return (
-    <>
-        <section className='mx-5 md:mx-12 lg:mx-24 bg-gray-900 rounded-lg text-white mt-10 lg:flex justify-between '>
+    <section className='mx-5 md:mx-12 lg:mx-24 bg-gray-900 rounded-lg text-white mt-10 p-5 lg:flex justify-between items-center'>
+      <div className="lg:w-1/3 py-5">
+        <h1 className="font-serif text-2xl pb-2 lg:pb-5">Get in Touch.</h1>
+        <h1 className="font-serif text-3xl font-semibold pb-2 lg:pb-5">Let’s Talk About Your <span className="text-light-blue-600">Next Project</span></h1>
+        <p className="md:text-xl">To discuss a project or just to say hi, feel free to connect with me via email, phone call, or through my social media handles.</p>
 
-            <div className="mx-5 lg:w-1/3 py-5"> 
-                <h1 className="font-serif text-2xl pb-2 lg:pb-5">Get in Touch.</h1>
-                <h1 className="font-serif text-3xl font-semibold pb-2 lg:pb-5">Let’s Talk For your <span className="text-light-blue-600">Next Projects</span></h1>
-                <p className="md:text-xl">To discuss a project or just to say hi, feel free to connect with me via email, phone call, or through my social media handles.</p>
+        <h1 className="md:text-xl my-2 flex items-center"> 
+          <span className="pr-5 text-3xl"><MdCall /></span>Tel: +254 726545820
+        </h1>
+        <h1 className="md:text-xl my-2 flex items-center"> 
+          <span className="pr-5 text-3xl"><MdOutlineMarkAsUnread/></span>Email: gilbert45ku@gmail.com
+        </h1>
 
-                <h1 className="md:text-xl my-2 flex "> <span className="pr-5 text-3xl"><MdCall /></span>Tel: +254 726545820</h1>
-                <h1 className="md:text-xl my-2 flex "> <span className="pr-5 text-3xl"><MdOutlineMarkAsUnread/></span>Email: gilbert45ku@gmail.com</h1>
+        {/* Social Icons */}
+        <div className="flex text-blue-500 mt-4 md:mt-8 space-x-3">
+          <FaGithub className="text-2xl md:text-3xl"/>
+          <FaLinkedinIn className="text-2xl md:text-3xl"/>
+          <FaTwitter className="text-2xl md:text-3xl"/>
+          <FaWhatsapp className="text-2xl md:text-3xl"/>
+        </div>
+      </div>
+      <div className="lg:w-2/3 mt-5 lg:mt-0">
+        <Form />
+      </div>
+    </section>
+  );
+};
 
-                {/* icons */}
-                <div className="flex text-blue-500 mt-1 md:mt-8 ">
-                    <FaGithub className="me-2 md:me-3 text-2xl text-blue-500"/>
-                    <FaLinkedinIn className="mx-2 md:mx-3 text-2xl md:text-3xl"/>
-                    <FaTwitter className="mx-2 md:mx-3 text-2xl md:text-3xl"/>
-                    <FaWhatsapp className="mx-2 md:mx-3 text-2xl md:text-3xl"/>
-                    <MdEmail className="ms-2 md:ms-3 text-2xl md:text-3xl "/>
-                </div>
-            </div>
-            <div className="md:mr-1 lg:mr-32 mx-5 lg:mx-0">
-                <Form />
-            </div>
-            
-        </section>
-    </>
-  )
-}
-
-export default ContactMe
+export default ContactMe;
--- components/contact/Form.jsx
+++ components/contact/Form.jsx
@@ -1,79 +1,75 @@
 "use client"
 import Image from "next/image";
 
-
 const Form = () => {
   return (
-    <>
-        <section className='relative '>
+    <section className='relative w-full h-[450px] lg:h-[350px]'>
+      {/* Image as Background */}
+      <Image 
+        src="/Vector.png"
+        layout="fill"
+        objectFit="contain"
+        alt="background image"
+        className="object-contain"
+      />  
+      
+      {/* Form positioned on top of the image */}
+      <form className="absolute top-0 left-0 right-0 bottom-0 flex flex-col justify-center items-center  ">
+        {/* Name */}
+        <div className="flex flex-wrap mb-6 w-full max-w-lg">
+          <div className="w-full px-3">
+            <input 
+              className="appearance-none block w-full text-white bg-white bg-opacity-25 border border-light-blue-500 rounded py-2 px-4 leading-tight focus:outline-none focus:bg-white focus:bg-opacity-50 focus:border-blue-500" 
+              type="text" 
+              placeholder="Full Name" 
+              name="full_name"
+              required
+            />
+          </div>
+        </div>  
+        {/* Phone and Email */}
+        <div className="flex flex-wrap mb-6 w-full max-w-lg">
+          <div className="w-full lg:w-1/2 px-3 mb-6 lg:mb-0">
+            <input 
+              className="appearance-none block w-full text-white bg-white bg-opacity-25 border border-light-blue-600 rounded py-2 px-4 leading-tight focus:outline-none focus:bg-white focus:bg-opacity-50 focus:border-blue-500"  
+              type="email" 
+              placeholder="Enter Email Address" 
+              required
+              name='email'
+            />
+          </div>
+          <div className="w-full lg:w-1/2 px-3">
+            <input 
+              className="appearance-none block w-full text-white bg-white bg-opacity-25 border border-light-blue-600 rounded py-2 px-4 leading-tight focus:outline-none focus:bg-white focus:bg-opacity-50 focus:border-blue-500" 
+              type="number"
+              placeholder="Enter Phone No"
+              required
+              name='phone'
+            />
+          </div>
+        </div>
+        {/* Message */}
+        <div className="w-full max-w-lg mb-6 px-3">
+          <textarea 
+            id="message" 
+            rows="4" 
+            className="block w-full p-2 text-sm text-white bg-white bg-opacity-25 rounded-lg border border-light-blue-600 focus:ring-blue-500 focus:outline-none focus:border-blue-500 focus:bg-white focus:bg-opacity-50" 
+            placeholder="Leave a Message..." 
+            required
+            name='message'
+          />
+        </div>
+        <div className='flex justify-center w-full px-3'>
+          <button 
+            type="submit" 
+            className="w-full lg:w-2/4 text-white bg-gradient-to-r from-cyan-300 to-light-blue-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm lg:text-xl px-5 py-2 text-center"
+          >
+            Send
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+};
 
-           <Image 
-                src="/Vector.png"
-                width={450}
-                height={350}
-                alt="profile image"
-                className="px-5 lg:pr-0 mt-5 lg:mt-0 items-center object-contain "
-                loading="lazy"
-            />  
-                
-            <form className="w-full max-w-lg absolute top-6  z-10" >
-                {/* name */}
-                <div className="flex flex-wrap -mx-3 mb-6">
-                    <div className="w-full px-3">
-                        <input className="appearance-none block w-full text-white bg-white bg-opacity-25 border border-light-blue-500 rounded py-2 px-4  leading-tight focus:outline-none focus:bg-white focus:bg-opacity-50 focus:border-blue-500" 
-                        type="text" 
-                        placeholder="Full Name" 
-                        name="full_name"
-                        // value={formData.full_name}
-                        // onChange={handleChange}
-                        required/>
-                    </div>
-                </div>  
-                {/* phone and email */}
-                <div className="flex flex-wrap -mx-3 mb-6">
-                    <div className="w-full lg:w-1/2 px-3 mb-6 lg:mb-0">
-                        <input className="appearance-none block w-full text-white bg-white bg-opacity-25 border border-light-blue-600 rounded py-2 px-4  leading-tight focus:outline-none focus:bg-white focus:bg-opacity-50 focus:border-blue-500"  
-                        type="email" 
-                        placeholder="Enter Email Address" 
-                        required
-                        name='email'
-                        // value={formData.email}
-                        // onChange={handleChange}
-                        />
-                    </div>
-                    <div className="w-full lg:w-1/2 px-3">
-                    <input className="appearance-none block w-full text-white bg-white bg-opacity-25 border border-light-blue-600 rounded py-2 px-4 leading-tight focus:outline-none focus:bg-white focus:bg-opacity-50 focus:border-blue-500" 
-                    type="number"
-                    placeholder="Enter Phone No"
-                    required
-                    name='phone'
-                    // value={formData.phone}
-                    // onChange={handleChange}
-                    />
-                    </div>
-                </div>
-                
-                {/* message */}
-                <div className=" mx-auto">
-                    <textarea id="message" rows="4" 
-                    className="block p-2 w-full text-sm text-black bg-white bg-opacity-25 rounded-lg border border-light-blue-600 focus:ring-blue-500 focus:outline-none focus:border-blue-500 focus:bg-white focus:bg-opacity-50  "
-                    placeholder="Leave a Message..." 
-                    required
-                    name='message'
-                    // value={formData.message}
-                    // onChange={handleChange}
-                    />
-                </div>
-                <div className='flex justify-end my-2'>
-                    <button type="submit" className="w-full lg:w-2/4 text-white bg-gradient-to-r from-cyan-300 to-light-blue-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300  font-medium rounded-lg text-sm lg:text-xl px-5 p-2 text-center ">
-                        Send
-                    </button>
-                </div>
-            </form>
-                
-        </section>
-    </>
-  )
-}
-
-export default Form
+export default Form;
