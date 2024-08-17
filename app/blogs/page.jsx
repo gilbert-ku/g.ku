e608b3c37cb97fb502105c9d15a79c3ed5046e18
gilbert-ku <gilbert45ku@gmail.com>
2024-08-18 01:07:08 +0300

    add Blogs components

--- app/blogs/page.jsx
+++ app/blogs/page.jsx
@@ -1,24 +1,26 @@
-import Image from "next/image"
+import Blogs from "@/components/Blogs/Blogs"
 
 
-const Blogs = () => {
+const BlogPage = () => {
   return (
     <>
-    <main className="min-h-[450px] md:min-h-[600px] mx-5 lg:mx-20 bg-no-repeat bg-cover md:rounded-3xl flex justify-center items-center bg-gradient-to-b from-white to-blue-gray-500" style={{ backgroundImage: `url("/blogbg.jpeg")` }}>
-      <div className="bg-black bg-opacity-70 p-6 md:p-12 md:rounded-3xl w-full max-w-screen-lg mx-auto text-white font-serif">
-        <h1 className="text-5xl md:text-7xl font-extrabold text-center">Welcome to My Blog</h1>
-        <p className="text-2xl md:text-3xl mt-4 text-center">Sharing insights, stories, and tips</p>
-        <p className="text-lg md:text-xl mt-8 leading-relaxed text-center">
-          I'm thrilled to have you here. Whether you're interested in personal stories, tech insights, or personal finance tips, this is the place for you. I share my experiences, knowledge, and thoughts on topics that I'm passionate about. So, grab a cup of coffee, dive in, and let's explore these journeys together!
-        </p>
-      </div>
-    </main>
+      <main className="min-h-[450px] md:min-h-[600px] mx-5 lg:mx-20 bg-no-repeat bg-cover md:rounded-3xl flex justify-center items-center bg-gradient-to-b from-white to-blue-gray-500" style={{ backgroundImage: `url("/blogbg.jpeg")` }}>
+        <div className="bg-black bg-opacity-70 p-6 md:p-12 md:rounded-3xl w-full max-w-screen-lg mx-auto text-white font-serif">
+          <h1 className="text-5xl md:text-7xl font-extrabold text-center">Welcome to My Blog</h1>
+          <p className="text-2xl md:text-3xl mt-4 text-center">Sharing insights, stories, and tips</p>
+          <p className="text-lg md:text-xl mt-8 leading-relaxed text-center">
+            I'm thrilled to have you here. Whether you're interested in personal stories, tech insights, or personal finance tips, this is the place for you. I share my experiences, knowledge, and thoughts on topics that I'm passionate about. So, grab a cup of coffee, dive in, and let's explore these journeys together!
+          </p>
+        </div>
+      </main>
+
+      <Blogs />
 
     </>
   )
 }
 
-export default Blogs
+export default BlogPage
 
 
 
