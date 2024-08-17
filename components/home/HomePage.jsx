04b8f983a409f40084cf71d98fb59b547d4e1670
gilbert-ku <gilbert45ku@gmail.com>
2024-08-18 01:24:03 +0300

    update home page

--- components/home/HomePage.jsx
+++ components/home/HomePage.jsx
@@ -13,42 +13,49 @@ const HomePage = () => {
         
         <div className="lg:flex justify-evenly my-4 md:my-12">
             <div className="px-5 md:px-8 lg:px-24 lg:w-1/2">
-              <h1 className="text-xl md:text-2xl">Hello, My Name is,</h1>
+              {/* <h1 className="text-xl md:text-2xl">Hello, My Name is,</h1> */}
 
-              <h1 className="text-2xl md:text-6xl font-bold">Gilbert <span className="text-light-blue-400" >Kutoto,</span></h1>
+              <h1 className="text-3xl md:text-6xl font-bold md:mb-3 text-center md:text-left">Gilbert <span className="text-light-blue-400" >Kutoto,</span></h1>
 
-              <TypeAnimation
-                sequence={[
-                  // Same substring at the start will only be typed once, initially
-                  'I am a Software Engineer,',
-                  1000,
-                  'I am a UX/UI Designer,',
-                  1000,
-                  'I am a Web Developer,',
-                  1000,
-                  'I am an Android/iOS Developer.',
-                  1000,
-                ]}
-                speed={40}
-                // style={{ fontSize: '2em' }}
-                className="text-xl md:text-2xl my-0 md:my-2"
-                repeat={Infinity}
-              />
-
-              <p className="lg:text-2xl my-2 lg:my-5">From Nairobi Kenya,I have a rich experience in development cycle form dynamic Web Application, 
+              <div className="flex justify-center md:justify-start">
+                <TypeAnimation
+                  sequence={[
+                    // Same substring at the start will only be typed once, initially
+                    'I am a Software Engineer,',
+                    1000,
+                    'I am a UX/UI Designer,',
+                    1000,
+                    'I am a Web Developer,',
+                    1000,
+                    'I am an Android/iOS Developer.',
+                    1000,
+                  ]}
+                  speed={30}
+                  // style={{ fontSize: '2em' }}
+                  className="text-xl md:text-4xl my-0 md:my-2 font-semibold text-center md:text-left"
+                  repeat={Infinity}
+                />
+              </div>
+              
+              <p className="md:text-2xl lg:text-3xl my-2 lg:my-4 text-center md:text-left">From Nairobi Kenya,I have a rich experience in development cycle form dynamic Web Application, 
                 Mobile App development and I am also proficient in UX/UI design.
               </p>
+                <div className="flex justify-center md:justify-start">
+                  <button className=" py-2 md:py-4 rounded-md font-semibold my-3 md:my-5 px-6 bg-gradient-to-r from-cyan-300 to-light-blue-600 md:text-xl">
+                  Download Resume
+                  </button>
+                </div>
 
-              <button className="p-2 rounded-md font-semibold my-3 nd:my-5 px-5 bg-gradient-to-r from-cyan-300 to-light-blue-600">Download Resume</button>
-
-              <div className="flex text-blue-500 mt-1 md:mt-3">
-                <FaGithub className="me-2 md:me-3 text-2xl text-blue-500"/>
-                <FaLinkedinIn className="mx-2 md:mx-3 text-2xl md:text-3xl"/>
-                <FaTwitter className="mx-2 md:mx-3 text-2xl md:text-3xl"/>
-                <FaWhatsapp className="mx-2 md:mx-3 text-2xl md:text-3xl"/>
-                <MdEmail className="ms-2 md:ms-3 text-2xl md:text-3xl "/>
-              </div>
-
+                <div className="flex justify-center md:justify-start">
+                  <div className="flex text-blue-500 mt-1 md:mt-3">
+                    <FaGithub className="me-2 md:me-3 text-2xl text-blue-500"/>
+                    <FaLinkedinIn className="mx-2 md:mx-3 text-2xl md:text-3xl"/>
+                    <FaTwitter className="mx-2 md:mx-3 text-2xl md:text-3xl"/>
+                    <FaWhatsapp className="mx-2 md:mx-3 text-2xl md:text-3xl"/>
+                    <MdEmail className="ms-2 md:ms-3 text-2xl md:text-3xl "/>
+                  </div>
+                </div>
+              
             </div>
             <Image 
             src="/profile-image.png"
