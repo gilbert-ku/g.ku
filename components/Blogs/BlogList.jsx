4a6b99ac357701575fdeb4b05a6b43932fafbd01
gilbert-ku <gilbert45ku@gmail.com>
2024-08-18 16:07:17 +0300

    stle blog cards

--- components/Blogs/BlogList.jsx
+++ components/Blogs/BlogList.jsx
@@ -38,26 +38,28 @@ const BlogList =  async() => {
     if (Array.isArray(blogs) && blogs.length > 0 ){
        return (
             <>
-            <h1 className="text-7xl text-white">hello Welcome</h1>
-                <section className="text-white">
+                <section className="text-white grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-x-11  gap-y-5 mt-3 mx-5 md:mx-12 lg:mx-24">
                     {
                         blogs.map((blog) => (
+                            
                             <div key={blog.uuid} className="text-white ">
-                                <Image 
-                                src={`http://127.0.0.1:8000${blog.image}`}
-                                width={500}
-                                height={400}
-                                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                                loading='lazy'
-                                alt={blog.title}
-                                />
-
-                                <h1 className="text-white text-4xl">{blog.title}</h1>
-
-                                <div className='md:ml-5 lg:ml-10 my-auto md:pr-5 lg:pr-3'>
+                                <div className="">
+                                    <Image 
+                                    src={`http://127.0.0.1:8000${blog.image}`}
+                                    width={500}
+                                    height={200}
+                                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                                    loading='lazy'
+                                    alt={blog.title}
+                                    className="object-scale-down w-full md:h-72 object-center"
+                                    />
+                                </div>
+                                
+                                <div className=' my-auto md:pr-5 lg:pr-3'>
                                     <h1 className='text-xl md:text-2xl text-light-blue-600 font-semibold mb-2 md:mb-5'>{blog.title}</h1>
-                                    <p className='md:text-xl lg:text-2xl'>{blog.blog_text}</p>
+                                    <p className='md:text-xl lg:text-2xl'>{blog.blog_text.slice(0, 200)}...</p>
                                     <p>date {blog.created_date}</p>
+                                    {/* <p>date {blog.user}</p> */}
                                 </div>
                             </div>
                         ))
