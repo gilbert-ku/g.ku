d764f5d64b36d3ba07d14c71145957fc27565e76
gilbert-ku <gilbert45ku@gmail.com>
2024-08-21 14:00:04 +0300

    update blod component

--- components/blogsComp/BlogList.jsx
+++ components/blogsComp/BlogList.jsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 // import ButtonLink from "./ButtonLink";
 import Link from 'next/link';
+import { GrLinkNext } from "react-icons/gr";
+
 
 
 
@@ -41,11 +43,11 @@ const BlogList =  async() => {
     if (Array.isArray(blogs) && blogs.length > 0 ){
        return (
             <>
-                <section className="text-white grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-x-11  gap-y-5 mt-3 mx-5 md:mx-12 lg:mx-24">
+                <section className="text-white grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-x-11  gap-y-5 mt-3 mx-5 md:mx-12 lg:mx-24 ">
                     {
                         blogs.map((blog) => (
                             
-                            <div key={blog.uuid} className="text-white ">
+                            <div key={blog.uuid} className="text-white rounded-xl px-2 py-1">
                                 <div className="">
                                     <Image 
                                     src={`http://127.0.0.1:8000${blog.image}`}
@@ -54,23 +56,24 @@ const BlogList =  async() => {
                                     sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                                     loading='lazy'
                                     alt={blog.title}
-                                    className="object-scale-down w-full md:h-72 object-center"
+                                    className="object-scale-down w-full md:h-72 object-center rounded-2xl md:px-5"
                                     />
                                 </div>
                                 
                                 <div className=' my-auto pe-2'>
                                     <h1 className='text-xl md:text-2xl text-light-blue-600 font-semibold mb-2 md:mb-5'>{blog.title}</h1>
                                     <p className='md:text-xl lg:text-2xl'>{blog.blog_text.slice(0, 200)}...</p>
-                                    <p>date {blog.created_date}</p>
+                                    {/* <p>date {blog.created_date}</p> */}
                                     {/* <p>date {blog.user}</p> */}
                                 </div>
 
-                                <Link href={`/blogs/${blog.uuid}`}>
-                                    Read more
-                                </Link>
+                                <div className="flex justify-between font-semibold my-2 pe-2">
+                                    <Link href={`/blogs/${blog.uuid}`}>
+                                    <button className="py-1 px-3 bg-gray-900 hover:bg-gradient-to-r from-cyan-300 to-light-blue-600 flex rounded-md"><span>Read more</span> <GrLinkNext className="my-auto ml-1"/></button>
+                                    </Link>
 
-                                
-
+                                    <p className="font-semibold my-auto">Posted <span className="text-light-blue-600">:</span> {blog.created_date}</p>
+                                </div>
                                 {/* <ButtonLink blog={blog} /> */}
                             </div>
                         ))
--- components/blogsComp/Blogs.jsx
+++ components/blogsComp/Blogs.jsx
@@ -5,10 +5,7 @@ const Blogs = () => {
   return (
     <>
         <div className='text-white mx-5 md:mx-12 lg:mx-28 md:mt-12'>
-          <h1 className='text-3xl  font-semibold font-serif'>
-            Recent 
-              <span className='text-light-blue-600'>Blog</span> 
-              Posts
+          <h1 className='text-3xl  font-semibold font-serif'>Recent <span className='text-light-blue-600'>Blog</span> Posts
               <span className='text-light-blue-600 text-4'>.</span>
           </h1>
         </div>
