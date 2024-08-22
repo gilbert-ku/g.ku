916a38d95ad3d1bb3aec8d3a502631197b4e69d7
gilbert-ku <gilbert45ku@gmail.com>
2024-08-22 23:51:23 +0300

    update next config with deployed api url

--- next.config.mjs
+++ next.config.mjs
@@ -12,11 +12,17 @@
 const nextConfig = {
   images: {
     remotePatterns: [
+      // {
+      //   protocol: 'http',
+      //   hostname: '127.0.0.1',
+      //   port: '8000', // Specify the port if your Django server is running on a specific port
+      //   pathname: '/media/**', // Adjust the path to match your media file structure
+      // },
+
       {
-        protocol: 'http',
-        hostname: '127.0.0.1',
-        port: '8000', // Specify the port if your Django server is running on a specific port
-        pathname: '/media/**', // Adjust the path to match your media file structure
+        protocol: 'https',
+        hostname: 'portfolio-backend-1kqj.onrender.com',
+        pathname: '/media/**', // Adjust the path if necessary
       },
     ],
   },
