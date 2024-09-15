5b433c4cfb43107dcd07c7e236cb004c1e9f9a1f
gilbert-ku <gilbert45ku@gmail.com>
2024-09-15 15:26:57 +0300

    add estateease mobile app

--- components/projects/Projects.jsx
+++ components/projects/Projects.jsx
@@ -2,7 +2,10 @@
 
 // import ProjectsLists  from './ProjectsLists'
 import { MdNavigateNext } from "react-icons/md";
+import Image from "next/image";
 import Link from "next/link";
+import { FaExternalLinkAlt } from "react-icons/fa";
+
 
 const Projects = () => {
   return (
@@ -14,6 +17,28 @@ const Projects = () => {
           Explore My Popular <span className='text-light-blue-600 '>Projects</span>
         </h1>
         {/* <ProjectsLists /> */}
+
+        {/* projects mobile app */}
+        <div className="md:flex">
+          <Image
+          src="/estateApp.png"
+          width={500}
+          height={300}
+          alt="EstateEase Mobile App"
+          loading="lazy"
+          className="w-full md:w-1/2"
+          />
+
+            <div className='md:ml-5 lg:ml-10 my-auto md:pr-5 lg:pr-3'>
+              <h1 className='text-xl md:text-2xl text-light-blue-600 font-semibold mb-2 md:mb-5'>EstateEase App (IOS/Android)</h1>
+              <p className='md:text-xl lg:text-2xl'>A real estate management platform with secure user authentication, property browsing, and listing capabilities. Tenants can manage leases, make online payments, and access invoices through a dedicated dashboard. Still under development.</p>
+              <button className='mt-3 md:mt-5 lg:mt-9 p-3 bg-blue-gray-900 w-full md:w-16 h-full md:h-16 rounded-full flex justify-center items-center align-bottom'>
+                {/* <h1 className='pr-4 my-auto font-serif font-bold text-xl text-light-blue-600 md:hidden'>go live</h1> */}
+                <Link href="https://github.com/gilbert-ku/EstateEase-mobileApp" target='_blank' className='text-light-blue-600 text-xl md:text-2xl font-semibold md:font-bold flex'> <h1 className='pr-4 my-auto font-serif font-bold text-xl text-light-blue-600 md:hidden'>go live</h1><FaExternalLinkAlt /></Link>
+              </button>
+            </div>
+        </div>
+        
         <div className='my-5 flex justify-center items-center'>
           <Link href="projects">
           <button className=" p-2 rounded-md font-semibold my-3 nd:my-5 px-5 bg-gradient-to-r from-cyan-300 to-light-blue-600 flex">View More Projects <MdNavigateNext className='my-auto font-extrabold'/></button>
