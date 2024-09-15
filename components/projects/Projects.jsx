b5b122ee5f4fd140a570f3e9655103a5a34f0046
gilbert-ku <gilbert45ku@gmail.com>
2024-09-15 16:33:51 +0300

    update link button

--- components/projects/Projects.jsx
+++ components/projects/Projects.jsx
@@ -19,7 +19,7 @@ const Projects = () => {
         {/* <ProjectsLists /> */}
 
         {/* projects mobile app */}
-        <div className="md:flex">
+        <div className="md:flex px-4 md:px-0">
           <Image
           src="/estateApp.png"
           width={500}
@@ -32,15 +32,16 @@ const Projects = () => {
             <div className='md:ml-5 lg:ml-10 my-auto md:pr-5 lg:pr-3'>
               <h1 className='text-xl md:text-2xl text-light-blue-600 font-semibold mb-2 md:mb-5'>EstateEase App (IOS/Android)</h1>
               <p className='md:text-xl lg:text-2xl'>A real estate management platform with secure user authentication, property browsing, and listing capabilities. Tenants can manage leases, make online payments, and access invoices through a dedicated dashboard. Still under development.</p>
-              <button className='mt-3 md:mt-5 lg:mt-9 p-3 bg-blue-gray-900 w-full md:w-16 h-full md:h-16 rounded-full flex justify-center items-center align-bottom'>
-                {/* <h1 className='pr-4 my-auto font-serif font-bold text-xl text-light-blue-600 md:hidden'>go live</h1> */}
-                <Link href="https://github.com/gilbert-ku/EstateEase-mobileApp" target='_blank' className='text-light-blue-600 text-xl md:text-2xl font-semibold md:font-bold flex'> <h1 className='pr-4 my-auto font-serif font-bold text-xl text-light-blue-600 md:hidden'>go live</h1><FaExternalLinkAlt /></Link>
-              </button>
+              
+            <Link href="https://github.com/gilbert-ku/EstateEase-mobileApp" target='_blank' 
+              className='text-light-blue-600 text-xl md:text-2xl font-semibold md:font-bold mt-1 md:mt-5 lg:mt-9 p-1 bg-blue-gray-900 w-full md:w-16 h-full md:h-16 rounded-2xl flex justify-center items-center align-bottom'> 
+                <button className='pr-4 my-auto font-serif font-bold text-xl text-light-blue-600 md:hidden'>go live</button><FaExternalLinkAlt />
+            </Link>
             </div>
         </div>
 
         {/* projects blue eye insurance */}
-        <div className="md:flex mt-3 md:mt-12">
+        <div className="md:flex mt-3 md:mt-12 px-4 md:px-0">
 
           {/* responsive */}
 
@@ -56,10 +57,14 @@ const Projects = () => {
           <div className='md:ml-5 lg:ml-0 my-auto md:pr-5 lg:pr-3'>
             <h1 className='text-xl md:text-2xl text-light-blue-600 font-semibold mb-2 md:mb-5'>Blue Eye Insurance Agency</h1>
             <p className='md:text-xl lg:text-2xl'>Blue Eye Insurance Agency is a web app for financial advisors that allows them to generate personalized leads, schedule meetings, and share financial content with clients through a blog section.</p>
-            <button className='mt-3 md:mt-5 lg:mt-9 p-3 bg-blue-gray-900 w-full md:w-16 h-full md:h-16 rounded-full flex justify-center items-center align-bottom'>
-              {/* <h1 className='pr-4 my-auto font-serif font-bold text-xl text-light-blue-600 md:hidden'>go live</h1> */}
+            {/* <button className='mt-3 md:mt-5 lg:mt-9 p-3 bg-blue-gray-900 w-full md:w-16 h-full md:h-16 rounded-full flex justify-center items-center align-bottom'>
+              <h1 className='pr-4 my-auto font-serif font-bold text-xl text-light-blue-600 md:hidden'>go live</h1>
               <Link href="https://blue-eye-insurance-agency.vercel.app/" target='_blank' className='text-light-blue-600 text-xl md:text-2xl font-semibold md:font-bold flex'> <h1 className='pr-4 my-auto font-serif font-bold text-xl text-light-blue-600 md:hidden'>go live</h1><FaExternalLinkAlt /></Link>
-            </button>
+            </button> */}
+            <Link href="https://blue-eye-insurance-agency.vercel.app/" target='_blank' 
+            className='text-light-blue-600 text-xl md:text-2xl font-semibold md:font-bold mt-1 md:mt-5 lg:mt-9 p-1 bg-blue-gray-900 w-full md:w-16 h-full md:h-16 rounded-2xl flex justify-center items-center align-bottom'> 
+              <button className='pr-4 my-auto font-serif font-bold text-xl text-light-blue-600 md:hidden'>go live</button><FaExternalLinkAlt />
+            </Link> 
           </div>
 
           <Image
@@ -73,7 +78,7 @@ const Projects = () => {
         </div>
         
         {/* estateease web app */}
-        <div className="md:flex mt-3 md:mt-12">
+        <div className="md:flex mt-3 md:mt-12 px-4 md:px-0">
           <Image
           src="/estateweb.png"
           width={500}
@@ -86,15 +91,16 @@ const Projects = () => {
             <div className='md:ml-5 lg:ml-10 my-auto md:pr-5 lg:pr-3'>
               <h1 className='text-xl md:text-2xl text-light-blue-600 font-semibold mb-2 md:mb-5'>EstateEase App (Web App)</h1>
               <p className='md:text-xl lg:text-2xl'>A real estate management platform with secure user authentication, property browsing, and listing capabilities. Tenants can manage leases, make online payments, and access invoices through a dedicated dashboard.</p>
-              <button className='mt-3 md:mt-5 lg:mt-9 p-3 bg-blue-gray-900 w-full md:w-16 h-full md:h-16 rounded-full flex justify-center items-center align-bottom'>
-                {/* <h1 className='pr-4 my-auto font-serif font-bold text-xl text-light-blue-600 md:hidden'>go live</h1> */}
-                <Link href="https://estateeaseapp.onrender.com/" target='_blank' className='text-light-blue-600 text-xl md:text-2xl font-semibold md:font-bold flex'> <h1 className='pr-4 my-auto font-serif font-bold text-xl text-light-blue-600 md:hidden'>go live</h1><FaExternalLinkAlt /></Link>
-              </button>
+              
+              <Link href="https://estateeaseapp.onrender.com/" target='_blank' 
+              className='text-light-blue-600 text-xl md:text-2xl font-semibold md:font-bold mt-1 md:mt-5 lg:mt-9 p-1 bg-blue-gray-900 w-full md:w-16 h-full md:h-16 rounded-2xl flex justify-center items-center align-bottom'> 
+                <button className='pr-4 my-auto font-serif font-bold text-xl text-light-blue-600 md:hidden'>go live</button><FaExternalLinkAlt />
+              </Link>
             </div>
         </div>
 
-        {/* projects blue eye insurance */}
-        <div className="md:flex mt-3 md:mt-12">
+        {/* projects stigma support */}
+        <div className="md:flex mt-3 md:mt-12 px-4 md:px-0">
 
           {/* responsive image */}
 
@@ -110,10 +116,13 @@ const Projects = () => {
           <div className='md:ml-5 lg:ml-0 my-auto md:pr-5 lg:pr-3'>
             <h1 className='text-xl md:text-2xl text-light-blue-600 font-semibold mb-2 md:mb-5'>HIV/AIDS Stigma Support Group</h1>
             <p className='md:text-xl lg:text-2xl'>Together, we can end the stigma! We envision a world where no one faces discrimination for having HIV/AIDS. We aim to foster compassion and understanding by sharing stories and raising voices. Join us in creating a stigma-free world.</p>
-            <button className='mt-3 md:mt-5 lg:mt-9 p-3 bg-blue-gray-900 w-full md:w-16 h-full md:h-16 rounded-full flex justify-center items-center align-bottom'>
+            {/* <button className='mt-3 md:mt-5 lg:mt-9 p-3 bg-blue-gray-900 w-full md:w-16 h-full md:h-16 rounded-full flex justify-center items-center align-bottom'> */}
               {/* <h1 className='pr-4 my-auto font-serif font-bold text-xl text-light-blue-600 md:hidden'>go live</h1> */}
-              <Link href="https://stigmasupport.onrender.com/" target='_blank' className='text-light-blue-600 text-xl md:text-2xl font-semibold md:font-bold flex'> <h1 className='pr-4 my-auto font-serif font-bold text-xl text-light-blue-600 md:hidden'>go live</h1><FaExternalLinkAlt /></Link>
-            </button>
+              <Link href="https://stigmasupport.onrender.com/" target='_blank' 
+              className='text-light-blue-600 text-xl md:text-2xl font-semibold md:font-bold mt-1 md:mt-5 lg:mt-9 p-1 bg-blue-gray-900 w-full md:w-16 h-full md:h-16 rounded-2xl flex justify-center items-center align-bottom'> 
+                <button className='pr-4 my-auto font-serif font-bold text-xl text-light-blue-600 md:hidden'>go live</button><FaExternalLinkAlt />
+              </Link>
+            {/* </button> */}
           </div>
 
           <Image
